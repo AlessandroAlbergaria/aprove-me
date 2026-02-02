95b0b47e6b69726eaed32088f726f74769cde008
Alessandro Albergaria <alessandroalbergariafilho16@hotmail.com>
2026-02-01 21:20:35 -0300

    Create PrismaService and PrismaModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration from './config/configuration';
+import { PrismaModule } from './database';
 import { IntegrationsModule } from './modules/integrations/integrations.module';
 import { PayableModule } from './modules/payable/payable.module';
 import { AssignorModule } from './modules/assignor/assignor.module';
@@ -13,6 +14,7 @@ import { AssignorModule } from './modules/assignor/assignor.module';
       isGlobal: true,
       load: [configuration],
     }),
+    PrismaModule,
     IntegrationsModule,
     PayableModule,
     AssignorModule,
